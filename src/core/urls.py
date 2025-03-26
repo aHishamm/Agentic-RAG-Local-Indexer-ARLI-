590e9e29e8f2d8662e9cd28fb548202680546561
Abdulrahman Al Muaitah <a.almuaitah@gmail.com>
2025-03-26 21:58:54 +0400

    Fixed routing to home page, accessible in port 8000

--- src/core/urls.py
+++ src/core/urls.py
@@ -1,12 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import redirect
-
-def redirect_to_search(request):
-    return redirect('search_files')
+from indexer.views import search_files
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('indexer/', include('indexer.urls')),
-    path('', redirect_to_search, name='home'),
+    path('', search_files, name='home'),  # Direct routing to search view
 ]
